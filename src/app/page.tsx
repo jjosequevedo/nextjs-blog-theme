1f1d694f5c5bf72f49ea114b61f12e0cc603560d
Jose Perez <jjose.quevedo@gmail.com>
2024-10-02 18:20:37 +0200

    Update cardlist component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Highlighted from "@/components/Highlighted";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { IArticle } from "./interfaces";
+import CardList from "@/components/CardList";
 
 const Home = () => {
 
@@ -23,7 +24,7 @@ const Home = () => {
       <main className="mt-10">
         <Highlighted articles={articles.slice(0, 2)} />
         <div className="block lg:flex lg:space-x-2 px-2 lg:p-0 mt-10 mb-10">
-          
+          <CardList articles={articles.slice(2, 7)} />
         </div>
       </main>
       <Footer />
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,6 @@
 import { CardListProps } from "@/app/interfaces";
+import dayjs from "dayjs";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -8,25 +10,19 @@ const CardList: React.FC<CardListProps> = ({ articles }) => {
         <div className="w-full lg:w-2/3">
             {
                 articles?.map((article, index) => (
-                    <Link className="block rounded w-full lg:flex mb-10" href="#">
-                        <div className="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75" style={{ backgroundImage: 'url()' }} title="deit is very important">
-                        </div>
+                    <Link key={index} className="block rounded w-full lg:flex mb-10" href="#">
+                        {article.image && <div className="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75" style={{ backgroundImage: `url(${article.image.url})` }} title={article.image.alt}></div>}
                         <div className="bg-white rounded px-4 flex flex-col justify-between leading-normal">
                             <div>
-                                <div className="mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2">
-                                    Aliquam venenatis nisl id purus rhoncus, in efficitur sem hendrerit.
-                                </div>
-                                <p className="text-gray-700 text-base">
-                                    Duis euismod est quis lacus elementum, eu laoreet dolor consectetur.
-                                    Pellentesque sed neque vel tellus lacinia elementum. Proin consequat ullamcorper eleifend.
-                                </p>
+                                <div className="mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2">{article.title}</div>
+                                {article.summary && <p className="text-gray-700 text-base">{article.summary.substring(0, 250)}</p>}
                             </div>
-                            <div className="flex mt-3">
-                                <img src="https://randomuser.me/api/portraits/men/86.jpg" className="h-10 w-10 rounded-full mr-2 object-cover">
-                                    <div>
-                                        <p className="font-semibold text-gray-700 text-sm capitalize"> eduard franz </p>
-                                        <p className="text-gray-600 text-xs"> 14 Aug </p>
-                                    </div>
+                            <div className="flex my-3">
+                                {article.image && <Image width={10} height={10} alt={article.image.alt} src={article.image.url} className="h-10 w-10 rounded-full mr-2 object-cover" />}
+                                <div>
+                                    <p className="font-semibold text-gray-700 text-sm capitalize">{article.author}</p>
+                                    <p className="text-gray-600 text-xs">{dayjs(article.changed).format('MMM DD, YYYY')}</p>
+                                </div>
                             </div>
                         </div>
                     </Link>
